4f0b44a9592dcecc8676b717039292d78289aac0
qre100g <kmnrddy4334@gmail.com>
2026-03-28 12:56:40 +0530

    Create a SummaryView

---
+++ CodeBreaker/UI/CodeBreakerSummary.swift
@@ -0,0 +1,19 @@
+//
+//  CodeBreakerSummary.swift
+//  CodeBreaker
+//
+//  Created by Mukesh Kondreddy on 28/03/26.
+//
+
+import SwiftUI
+
+struct CodeBreakerSummary: View {
+    var body: some View {
+        CodeBreakerView()
+    }
+}
+
+
+#Preview {
+    CodeBreakerSummary()
+}
