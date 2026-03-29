b415b1348cf9d8ae7b8775f11ab77c4f0ae39e49
qre100g <kmnrddy4334@gmail.com>
2026-03-29 13:40:30 +0530

    Append the models to the game instead of assigning to the array.

--- CodeBreaker/UI/GameChooser.swift
+++ CodeBreaker/UI/GameChooser.swift
@@ -28,9 +28,25 @@ struct GameChooser: View {
             .listStyle(.plain)
         }
         .onAppear {
+<<<<<<< HEAD
             games.append(CodeBreaker(name: "First Game"))
             games.append(CodeBreaker(name: "Second Game"))
             games.append(CodeBreaker(name: "Third Game"))
+=======
+<<<<<<< HEAD:CodeBreaker/UI/GameChooser.swift
+            games.append(CodeBreaker(name: "First Game"))
+            games.append(CodeBreaker(name: "Second Game"))
+            games.append(CodeBreaker(name: "Third Game"))
+=======
+            if games.isEmpty {
+                games = [
+                    CodeBreaker(name: "First Game"),
+                    CodeBreaker(name: "Second Game"),
+                    CodeBreaker(name: "Third Game")
+                ]
+            }
+>>>>>>> 92cf32a (Add a third game):CodeBreaker/UI/CodeBreakerSummary.swift
+>>>>>>> cfca396 (Append the models to the game instead of assigning to the array.)
         }
     }
 }
